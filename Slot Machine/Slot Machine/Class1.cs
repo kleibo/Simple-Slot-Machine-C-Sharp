7798ee9d83812804e859215ce391ba5f75ade87c
kleibo <kleibo1999@gmail.com>
2023-01-24 23:35:13 -0500

    Added class for monte carlo simulation

---
+++ Slot Machine/Slot Machine/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class Simulation : Program
+    {
+        public int TotalWinnings { get; set; }
+        public int TotalBets { get; set; }
+        public int TotalPlays { get; set; }
+        public int[,] Reels { get; set; }
+
+        public Simulation(int TotalPlays)
+        {
+            TotalWinnings = 0;
+            TotalBets = 0;
+            this.TotalPlays = TotalPlays;
+            Reels = new int[3, 3];
+        }
+
+        public void MonteCarloSimulation()
+        {
+            for (int i = 0; i < TotalPlays; i++)
+            {
+                // Spin the reels
+                SpinReels(Reels);
+                int bet = 10;
+                TotalBets += bet;
+                // Check for winning combinations
+                int winnings = CheckWin(Reels, bet);
+                TotalWinnings+= winnings;
+            }
+        }
+
+        public void PrintResults()
+        {
+            // Print the results of the simulation
+            Console.WriteLine("Total winnings: " + TotalWinnings);
+            Console.WriteLine("Total bets: " + TotalBets);
+            Console.WriteLine("Average winnings per play: " + (double)TotalWinnings/ TotalPlays);
+            Console.WriteLine("Probability of winning: " + (double)TotalWinnings / TotalBets);
+        }
+    }
+}
--- Slot Machine/Slot Machine/Program.cs
+++ Slot Machine/Slot Machine/Program.cs
@@ -11,68 +11,74 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
-            // Initialize variables
-            int balance = 100;
-            int bet = 0;
-            int winnings = 0;
-            int[,] reels = new int[3,3];
-            string playAgain = "y";
+            //    // Initialize variables
+            //    int balance = 100;
+            //    int bet = 0;
+            //    int winnings = 0;
+            //    int[,] reels = new int[3,3];
+            //    string playAgain = "y";
 
-            // Welcome message
-            Console.WriteLine("Welcome to the Slot Machine!");
-            Console.WriteLine("Your current balance is: " + balance);
+            //    // Welcome message
+            //    Console.WriteLine("Welcome to the Slot Machine!");
+            //    Console.WriteLine("Your current balance is: " + balance);
 
-            // Main game loop
-            while (playAgain == "y")
-            {   
-                // Get bet amount from user
-                Console.WriteLine("Enter bet amount: ");
-                bet = int.Parse(Console.ReadLine());
+            //    // Main game loop
+            //    while (playAgain == "y")
+            //    {   
+            //        // Get bet amount from user
+            //        Console.WriteLine("Enter bet amount: ");
+            //        bet = int.Parse(Console.ReadLine());
 
-                // Check if bet amount is valid
-                if (bet > balance)
-                {
-                    Console.WriteLine("Invalid bet amount. Please enter a valid amount.");
-                }
-                else
-                {
-                    // Spin the reels
-                    SpinReels(reels);
+            //        // Check if bet amount is valid
+            //        if (bet > balance)
+            //        {
+            //            Console.WriteLine("Invalid bet amount. Please enter a valid amount.");
+            //        }
+            //        else
+            //        {
+            //            // Spin the reels
+            //            SpinReels(reels);
 
-                    // Check for winning combinations
-                    winnings = CheckWin(reels, bet);
+            //            // Check for winning combinations
+            //            winnings = CheckWin(reels, bet);
 
-                    // Update balance
-                    balance -= bet;
-                    balance += winnings;
+            //            // Update balance
+            //            balance -= bet;
+            //            balance += winnings;
 
 
-                    // Print results
-                    Console.WriteLine("Spin results: ");
-                    for (int i = 0; i < 3; i++)
-                    {
-                        for (int j = 0; j < 3; j++)
-                        {
-                            Console.Write(reels[i, j] + " ");
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine("Winnings: " + winnings);
-                    Console.WriteLine("New balance: " + balance);
+            //            // Print results
+            //            Console.WriteLine("Spin results: ");
+            //            for (int i = 0; i < 3; i++)
+            //            {
+            //                for (int j = 0; j < 3; j++)
+            //                {
+            //                    Console.Write(reels[i, j] + " ");
+            //                }
+            //                Console.WriteLine();
+            //            }
+            //            Console.WriteLine("Winnings: " + winnings);
+            //            Console.WriteLine("New balance: " + balance);
 
-                }
+            //        }
 
-                // Ask if player wants to play again
-                Console.WriteLine("Do you want to play again? (y/n)");
-                playAgain = Console.ReadLine();
-            }
+            //        // Ask if player wants to play again
+            //        Console.WriteLine("Do you want to play again? (y/n)");
+            //        playAgain = Console.ReadLine();
+            //    }
 
-            // Goodbye message
-            Console.WriteLine("Thanks for playing! Goodbye!");
+            //    // Goodbye message
+            //    Console.WriteLine("Thanks for playing! Goodbye!");
+            //
+            Simulation sim = new Simulation(10000);
+            sim.MonteCarloSimulation();
+            sim.PrintResults();
+
+
         }
 
         // Method to spin the reels
-        static void SpinReels(int[,] reels)
+        public static void SpinReels(int[,] reels)
         {
             Random rand = new Random();
             for (int i = 0; i < 3; i++)
@@ -85,7 +91,7 @@ namespace SlotMachine
         }
 
         // Method to check for winning combinations and calculate winnings
-        static int CheckWin(int[,] reels, int bet)
+        public static int CheckWin(int[,] reels, int bet)
         {
             int winnings = 0;
             bool colWin = false;
