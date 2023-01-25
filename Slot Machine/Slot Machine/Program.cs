d4716cd62c43c9a06ffa58f2588d42aea2d44744
kleibo <kleibo1999@gmail.com>
2023-01-24 20:38:34 -0500

    Slot template created

--- Slot Machine/Slot Machine/Program.cs
+++ Slot Machine/Slot Machine/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize variables
+            int balance = 100;
+            int bet = 0;
+            int winnings = 0;
+            int[] reels = new int[3];
+            string playAgain = "y";
+
+            // Welcome message
+            Console.WriteLine("Welcome to the Slot Machine!");
+            Console.WriteLine("Your current balance is: " + balance);
+
+            // Main game loop
+            while (playAgain == "y")
+            {
+                // Get bet amount from user
+                Console.WriteLine("Enter bet amount: ");
+                bet = int.Parse(Console.ReadLine());
+
+                // Check if bet amount is valid
+                if (bet > balance)
+                {
+                    Console.WriteLine("Invalid bet amount. Please enter a valid amount.");
+                }
+                else
+                {
+                    // Spin the reels
+                    
+
+                    // Check for winning combinations
+                    
+
+                    // Update balance
+                    
+
+                    // Print results
+                    Console.WriteLine("Spin results: " + reels[0] + " " + reels[1] + " " + reels[2]);
+                    Console.WriteLine("Winnings: " + winnings);
+                    Console.WriteLine("New balance: " + balance);
+                }
+
+                // Ask if player wants to play again
+                Console.WriteLine("Do you want to play again? (y/n)");
+                playAgain = Console.ReadLine();
+            }
+
+            // Goodbye message
+            Console.WriteLine("Thanks for playing! Goodbye!");
+        }
